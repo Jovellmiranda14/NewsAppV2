14569d2f70580fca98b56bda036ec97d7d90c987
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2025-07-01 22:03:49 +0800

    fix: adjust Header component font size and margin for better alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 const headerStyle = {
   fontFamily: '"Baskerville Old Face", serif',
   fontWeight: 100,
-  fontSize: "70px",
+  fontSize: "64px",
   textAlign: "center",
   color: "#a10000",
 };
 
 const Header = () => (
-  <header className="text-center py-4 mt-3 border-bottom">
+  <header className="text-center py-4 mt-1 border-bottom">
     <h1 className="display-4" style={headerStyle}>
       REEL<span className="text-dark">NEWS</span>
     </h1>

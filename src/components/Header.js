46a138e80516695739bbff1f693b5eb47170877e
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2025-07-01 20:32:21 +0800

    feat: add Header component with styled title

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const headerStyle = {
+  fontFamily: '"Baskerville Old Face", serif',
+  fontWeight: 100,
+  fontSize: "70px",
+  textAlign: "center",
+  color: "#a10000",
+};
+
+const Header = () => (
+  <header className="text-center py-4 mt-2 border-bottom">
+    <h1 className="display-4" style={headerStyle}>
+      REEL<span className="text-dark">NEWS</span>
+    </h1>
+  </header>
+);
+export default Header;

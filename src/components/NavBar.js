a89555da66e2f2627beaea795b0008f8ed2da117
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2025-07-01 21:02:15 +0800

    refactor: adjust padding in NavBar component for improved layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
   const navItems = ["Home", "Posts", "Categories", "Technology", "Contact"];
 
   return (
-    <nav className="navbar navbar-custom-bg text-white py-4 px-0 w-100">
+    <nav className="navbar navbar-custom-bg text-white py-3 px-0 w-100">
       <div
         className="d-flex justify-content-center align-items-center w-100"
         style={{ gap: "45px", marginLeft: "50px" }}
